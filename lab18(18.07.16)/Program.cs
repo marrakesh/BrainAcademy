519c19d9a25bda5dedf136f484939411c023408f
Alekseii Ozerov <alekseiiozerov@gmail.com>
2016-07-20 15:28:22 +0300

    18thlab

--- lab18(18.07.16)/Program.cs
+++ lab18(18.07.16)/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Example_1_FileStream
 {

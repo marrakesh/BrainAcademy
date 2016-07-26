1d5524d43904c687ad69013808df1e19a0388ec0
Alekseii Ozerov <alekseiiozerov@gmail.com>
2016-07-26 16:37:19 +0300

    lab13

---
+++ lab13(29.06.16)1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("BrainAcademy.Student");
+            Console.WriteLine("Properties by reflection");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach(PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.Name);
+            }
+
+            Console.WriteLine("Methods by reflection");
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
+   
+}
+namespace BrainAcademy
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+
+        public Student()
+        {
+            this.Id = -1;
+            this.name = "Unknown";
+            this.lastname = "Unknown";
+        }
+
+        public Student(int Id, string name, string lastname)
+        {
+            this.Id = Id;
+            this.name = name;
+            this.lastname = lastname;
+        }
+
+        public void printId()
+        {
+            Console.WriteLine("Id : {0}", Id);
+        }
+
+        public void fullName()
+        {
+            Console.WriteLine("Full Name : {0} {1}", name, lastname);
+        }
+    }
+}
---
+++ lab13(29.06.16)2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirplaneLibrary.Classes;
+using AirplaineLibrary;
+using System.Reflection;
+
+namespace AirplaineLibrary
+{
+    public enum AirplainTypes {SportPlane, CargoPlane, TurboProp, Jet };
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true, Inherited = false)]
+    public sealed class AirplaneTypeAttribute : Attribute
+    {
+        public AirplainTypes Type { get; set; }
+
+        public AirplaneTypeAttribute()
+        {
+            Type = AirplainTypes.TurboProp;
+        }
+        public AirplaneTypeAttribute(AirplainTypes Type)
+        {
+            this.Type = Type;
+        }
+
+        public static implicit operator AirplaneTypeAttribute(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type type = Type.GetType("AirplaneLibrary.Classes.UniversalAirplane");
+            //PropertyInfo[] prop = type.GetProperties();
+            //foreach (PropertyInfo pro in prop)
+            //{
+            //    Console.WriteLine(pro.Name);
+            //}
+            //Type type1 = Type.GetType("AirplaineLibrary.AirplaneTypeAttribute");
+            //PropertyInfo[] prop1 = type1.GetProperties();
+            //foreach(PropertyInfo pro in prop1)
+            //{
+            //    Console.WriteLine(pro.Name);
+            //}
+
+            Attribute[] asa =Attribute.GetCustomAttributes(typeof(UniversalAirplane), typeof(AirplaneTypeAttribute));
+            foreach (var attr in asa) {
+                AirplaneTypeAttribute airTypeAttr = (AirplaneTypeAttribute)attr;
+                Console.WriteLine(airTypeAttr.Type);    
+            }
+
+            Attribute[] asa1 = Attribute.GetCustomAttributes(typeof(CargoAirplane), typeof(AirplaneTypeAttribute));
+            foreach(var attr in asa1)
+            {
+                AirplaneTypeAttribute airt = (AirplaneTypeAttribute)attr;
+                Console.WriteLine(airt.Type);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+namespace AirplaneLibrary.Classes
+{
+
+    [AirplaneType(AirplainTypes.CargoPlane)]
+    [AirplaneType(AirplainTypes.Jet)]
+
+    class UniversalAirplane
+    {
+        public int Speed {get; set;}
+        public string Name  {get; set;}
+        public UniversalAirplane()
+        {
+            Console.WriteLine("Universal");
+        }
+    }
+
+    [AirplaneType(AirplainTypes.CargoPlane)]
+    class CargoAirplane
+    {
+        public int Speed;
+        public string Name;
+        public CargoAirplane()
+        {
+            Console.WriteLine("Cargo");
+        }
+    }
+}
